079396eebc49931a7e4a19f1bca4f8a6842160e2
Sergey Balukov <bserghey@yandex.ru>
2022-08-12 10:11:04 +0300

    All task lesson3

--- Lesson2/Task1/Program.cs
+++ Lesson2/Task1/Program.cs
@@ -2,4 +2,4 @@
 Console.Write("Введи трёхзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра числа -> "+stringNumber[1]);
+Console.WriteLine("вторая цифра числа -> " + stringNumber[1]);
---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введи номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Этот день выходной");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Такого дня не существует");
+    }
+    else Console.WriteLine("Этот день не выходной");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
